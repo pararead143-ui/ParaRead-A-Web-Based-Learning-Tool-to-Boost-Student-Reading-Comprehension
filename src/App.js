332c8fa85b502c0528864a00103ad64b0a1364af
pararead143-ui <pararead143@gmail.com>
2025-11-01 16:37:33 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import Home from "./components/Homepage";
+import Homepage from "./components/Homepage";
+import SummarizationPage from "./components/SummarizationPage";
+
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginRedirect />} />
-        <Route path="/login" element={<LoginRedirect />} /> {/* 👈 ADD THIS */}
+        {/* Default route ("/") → goes to Login */}
+        <Route path="/" element={<Navigate to="/login" />} />
+
+        {/* Login, Signup, and Homepage routes */}
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/homepage" element={<Home />} />
+        <Route path="/home" element={<Homepage />} />
+         <Route path="/summary" element={<SummarizationPage />} />
       </Routes>
+      
     </Router>
   );
-}
-
-function LoginRedirect() {
-  const navigate = useNavigate();
-  return <Login onLogin={() => navigate("/home")} />;
 }
 
 export default App;
