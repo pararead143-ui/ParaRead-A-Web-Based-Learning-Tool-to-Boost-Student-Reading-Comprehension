961bf9fcaee6a332ef42f76d32e00ea3fb7aa26b
pararead143-ui <pararead143@gmail.com>
2025-11-01 16:29:58 +0800

    Update models.py

--- readings/models.py
+++ readings/models.py
@@ -1,7 +1,7 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
-from django.db import models
+
 
 class Passage(models.Model):
     title = models.CharField(max_length=200)
@@ -9,3 +9,39 @@ class Passage(models.Model):
 
     def __str__(self):
         return self.title
+
+class ReadingMaterial(models.Model):
+    user = models.ForeignKey(
+    settings.AUTH_USER_MODEL,
+    on_delete=models.CASCADE,
+    related_name="reading_materials_readings"
+)
+
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True, null=True)
+    file = models.FileField(upload_to='uploads/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class SegmentedPart(models.Model):
+    reading = models.ForeignKey(ReadingMaterial, on_delete=models.CASCADE, related_name="segments")
+    segment_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Segment of {self.reading.title}"
+
+class Summary(models.Model):
+    reading = models.ForeignKey(
+        'ReadingMaterial',
+        on_delete=models.CASCADE,
+        related_name='summaries'
+    )
+    summary_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Summary of {self.reading.title}"
